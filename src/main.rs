edf8f48a17d0a3b1309834f2c55176583e03600a
takechee <takechee14@gmail.com>
2021-02-13 14:07:18 +0900

    add: read_dir

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use std::fs;
+use std::io;
+use std::path::Path;
+use chrono::{DateTime, TimeZone, Local};
+
+fn main() {
+    read_dir("./");
+}
+
+fn read_dir<P: AsRef<Path>>(path: P) -> io::Result<String> {
+    let entries = fs::read_dir(path)?;
+    for entry in entries {
+        let entry = entry?;
+        let metadata = entry.metadata()?;
+        if metadata.is_dir() {} else {
+            let modified = metadata.modified();
+            if modified.is_err() {
+                continue;
+            }
+
+            let duration = modified.unwrap().duration_since(
+                std::time::SystemTime::UNIX_EPOCH
+            );
+
+            if duration.is_err() {
+                continue;
+            }
+
+            let datetime: DateTime<Local> = Local.timestamp(duration.unwrap().as_secs() as i64, 0);
+            println!("{}, {}", datetime, entry.path().display().to_string());
+        }
+    }
+    Ok(String::from("OK"))
+}
